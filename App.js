ece5a2b8e095faa402c927e5c8dd5403914dc1c9
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 16:47:46 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from "./screens/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Calls 🔥</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
