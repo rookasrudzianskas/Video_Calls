e76060fc08dd7ff6268a79a090f2d58e9ae8991e
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 19:02:07 +0300

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <View style={styles.container}>
             <Entypo name="notification" size={30} color="#efefef" />
+            <Text>Meet & Chat</Text>
             <Entypo name="new-message" size={30} color="#efefef" />
         </View>
     );
@@ -17,4 +18,9 @@ const styles = StyleSheet.create({
     container: {
 
     },
+    heading: {
+        color: '#efefef',
+        fontSize: 20,
+        fontWeight: '700',
+    }
 });
