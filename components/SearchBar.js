42fadec70de3fda2858be9725a9c69e3714c6733
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 19:10:00 +0300

    Rokas

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import Fontisto from "react-native-vector-icons/Fontisto";
 
 const SearchBar = () => {
     return (
         <View style={styles.container}>
-
+            <Fontisto name="search" size={20} color="#858585" />
+            <Text style={styles.textSearchBar}>Search</Text>
         </View>
     );
 };
@@ -14,6 +16,14 @@ export default SearchBar;
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: '#333333',
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        height: 40,
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+    textSearchBar: {
 
-    }
+    },
 });
