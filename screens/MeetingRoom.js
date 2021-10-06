be2f3295b98b7e7fdbc3199a9f9787a870842af3
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 19:59:00 +0300

    ROkas

---
+++ screens/MeetingRoom.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const MeetingRoom = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default MeetingRoom;
