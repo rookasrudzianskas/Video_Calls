89054f6b41e881b1997b8952a4278299b6f5eade
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 19:33:30 +0300

    Rokas

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 const ContactsMenu = () => {
     return (
         <View>
-            <Text>
-                byrookas 🚀
-            </Text>
+
         </View>
     );
 };
