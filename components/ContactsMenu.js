09d88b86ddc0251a15d22aa841633c8a07506d0f
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 19:39:54 +0300

    Rokas

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 const ContactsMenu = () => {
     return (
         <View style={styles.container}>
-
+             <TouchableOpacity activeOpacity={0.8}>
+                <View style={styles.row}>
+                    <View style={styles.starredIcon}>
+                        <AntDesign name="star" size={24} color="#efefef" />
+                    </View>
+                    <Text style={styles.text}>
+                        Start
+                    </Text>
+                </View>
+             </TouchableOpacity>
         </View>
     );
 };
