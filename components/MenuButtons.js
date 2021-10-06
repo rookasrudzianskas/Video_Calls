4caf04246c73b099ed35804bceb13a362933bcc1
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 19:28:21 +0300

    Rokas

--- components/MenuButtons.js
+++ components/MenuButtons.js
@@ -52,6 +52,7 @@ const styles = StyleSheet.create({
     },
     buttonContainer: {
         alignItems: 'center',
+        // justifyContent: 'space-between',
     },
     menuText: {
         color: '#858585',
