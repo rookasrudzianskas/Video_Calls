a01fb0cf308c10bd99cd41e6be4583aba97cc7c0
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 19:16:23 +0300

    ROkas

--- components/MenuButtons.js
+++ components/MenuButtons.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 const MenuButtons = () => {
     return (
         <View style={styles.container}>
             <View style={styles.buttonContainer}>
                 <TouchableOpacity>
-
+                    <FontAwesome name="video-camera" size={23} color="#efefef" />
+                    <Text>New Meeting</Text>
                 </TouchableOpacity>
             </View>
         </View>
