ddb4b5a0ab9cdb5401cdf10c620c03ab6b1aa833
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 16:53:36 +0300

    Roaks

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Header = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default Header;
---
+++ components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const SearchBar = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default SearchBar;
