3cb369c669fffb46dc63c25bbb9291d727506ba2
Rokas <rookas.rudzenskas@gmail.com>
2021-10-07 09:14:51 +0300

    Rokas

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet, SafeAreaView, TextInput} from 'react-native';
 
 const MeetingRoom = () => {
+    const [name, setName] = useState();
+
     return (
-            <View>
+            <View style={styles.container}>
                 <View style={styles.startMeetingContainer}>
                     <View style={styles.info}>
-                        <TextInput placeholder="Enter name"/>
+                        <TextInput placeholder="Enter name" style={styles.textInput} value={name} onChangeText={text => setName(text)}/>
                     </View>
                     <View style={styles.info}>
-                        <TextInput placeholder="Enter room id" />
+                        <TextInput placeholder="Enter room id" style={styles.textInput} value={name} onChangeText={text => setName(text)} />
                     </View>
                 </View>
             </View>
@@ -20,12 +22,16 @@ export default MeetingRoom;
 
 const styles = StyleSheet.create({
     container: {
-
+        backgroundColor: '#1c1c1c',
+        flex: 1,
     },
     startMeetingContainer: {
 
     },
     info: {
+
+    },
+    textInput: {
 
     }
 });
