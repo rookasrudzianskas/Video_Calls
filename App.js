a173e9c5057b10be2b802391f9d1edcc0e6e8430
Rokas <rookas.rudzenskas@gmail.com>
2021-10-07 15:37:37 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Home from "./screens/Home";
 import Navigation from "./navigation";
+import { Camera } from 'expo-camera';
+
 
 export default function App() {
+
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    const [startCamera, setStartCamera] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
   return (
       <>
         <Navigation />
