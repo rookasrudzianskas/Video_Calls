93fb2a20d1ecc9eacd8f264b8ee7b4c42899eb8e
Rokas <rookas.rudzenskas@gmail.com>
2021-10-07 08:50:40 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Home from "./screens/Home";
+import Navigation from "./navigation";
 
 export default function App() {
   return (
       <View>
-        <Home />
+        <Navigation />
         <StatusBar style="auto" />
       </View>
   );
