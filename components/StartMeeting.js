51473e1ec8c2151570d4fb844778c1cd46e694a1
Rokas <rookas.rudzenskas@gmail.com>
2021-10-07 09:56:41 +0300

    Rokas

---
+++ components/StartMeeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const StartMeeting = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default StartMeeting;
