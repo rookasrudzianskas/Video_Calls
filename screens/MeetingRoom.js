107953ca999ac8c87da9f45293c8ebaec9953d64
Rokas <rookas.rudzenskas@gmail.com>
2021-10-07 09:10:52 +0300

    Rokas

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -3,14 +3,26 @@ import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
 
 const MeetingRoom = () => {
     return (
-        <SafeAreaView>
+            <View>
+                <View style={styles.startMeetingContainer}>
+                    <View style={styles.info}>
 
-        </SafeAreaView>
+                    </View>
+                </View>
+            </View>
     );
 };
 
 export default MeetingRoom;
 
 const styles = StyleSheet.create({
+    container: {
 
+    },
+    startMeetingContainer: {
+
+    },
+    info: {
+
+    }
 });
