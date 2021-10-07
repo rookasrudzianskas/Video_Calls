84cc6e23ea3a1fcfe58860dc0dbbdbe1003ca149
Rokas <rookas.rudzenskas@gmail.com>
2021-10-07 09:59:37 +0300

    Rokas

--- components/StartMeeting.js
+++ components/StartMeeting.js
@@ -1,12 +1,69 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 const StartMeeting = () => {
+
+
+
     return (
         <View>
-
+            {/* start meeting */}
+            <View style={styles.startMeetingContainer}>
+                <View style={styles.info}>
+                    <TextInput placeholder="Enter name" placeholderTextColor={'#767476'} style={styles.textInput} value={name} onChangeText={text => setName(text)}/>
+                </View>
+                <View style={styles.info}>
+                    <TextInput placeholder="Enter room id"  placeholderTextColor={'#767476'} style={styles.textInput} value={roomId} onChangeText={text => setRoomId(text)} />
+                </View>
+            </View>
+            {/*    button   */}
+            <View style={{alignItems: 'center',}}>
+                <TouchableOpacity onPress={() => {}} style={styles.startMeetingButton}>
+                    <Text style={styles.startMeetingText}>Start Meeting</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 export default StartMeeting;
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#1c1c1c',
+        flex: 1,
+    },
+    startMeetingContainer: {
+
+    },
+    info: {
+        width: '100%',
+        backgroundColor: '#373538',
+        height: 50,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: '#484648',
+        padding: 20,
+        justifyContent: 'center',
+    },
+    textInput: {
+        color: 'white',
+        fontSize: 18,
+        height: 15,
+    },
+    startMeetingButton: {
+        width: 350,
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#0470dc',
+        height: 50,
+        borderRadius: 15,
+    },
+    startMeetingText: {
+        color: 'white',
+        fontWeight: 'bold',
+    }
+});
+
--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Text, View, StyleSheet, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
+import StartMeeting from "../components/StartMeeting";
 
 const MeetingRoom = () => {
     const [name, setName] = useState();
@@ -7,21 +8,7 @@ const MeetingRoom = () => {
 
     return (
             <View style={styles.container}>
-                {/* start meeting */}
-                <View style={styles.startMeetingContainer}>
-                    <View style={styles.info}>
-                        <TextInput placeholder="Enter name" placeholderTextColor={'#767476'} style={styles.textInput} value={name} onChangeText={text => setName(text)}/>
-                    </View>
-                    <View style={styles.info}>
-                        <TextInput placeholder="Enter room id"  placeholderTextColor={'#767476'} style={styles.textInput} value={roomId} onChangeText={text => setRoomId(text)} />
-                    </View>
-                </View>
-            {/*    button   */}
-                <View style={{alignItems: 'center',}}>
-                    <TouchableOpacity onPress={() => {}} style={styles.startMeetingButton}>
-                        <Text style={styles.startMeetingText}>Start Meeting</Text>
-                    </TouchableOpacity>
-                </View>
+                <StartMeeting name={name} roomId={roomId} setName={setName} setRoomId={setRoomId} />
             </View>
     );
 };
@@ -33,35 +20,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#1c1c1c',
         flex: 1,
     },
-    startMeetingContainer: {
-
-    },
-    info: {
-        width: '100%',
-        backgroundColor: '#373538',
-        height: 50,
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        borderColor: '#484648',
-        padding: 20,
-        justifyContent: 'center',
-    },
-    textInput: {
-        color: 'white',
-        fontSize: 18,
-        height: 15,
-    },
-    startMeetingButton: {
-        width: 350,
-        marginTop: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#0470dc',
-        height: 50,
-        borderRadius: 15,
-    },
-    startMeetingText: {
-        color: 'white',
-        fontWeight: 'bold',
-    }
 });
+
