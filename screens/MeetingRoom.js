887288eea6fd39a272a642c10dc61264fcde85d1
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 09:57:43 +0300

    Rokas

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -66,7 +66,7 @@ const MeetingRoom = () => {
             <View style={styles.container}>
                 {startCamera ? (
                     <SafeAreaView style={{flex: 1,}}>
-                            <View style={{flex: 1, backgroundColor: 'black', justifyContent: 'center'}}>
+                            <View style={{flex: 1, backgroundColor: 'black', justifyContent: 'center', flexDirection: 'row', flexWrap: 'wrap'}}>
                                 <Camera style={{height: activeUsers?.length === 0 ? 600 : 200, width: activeUsers?.length === 0 ? "100%" : 200 }} type={type}>
                                     <View style={styles.buttonContainer}>
                                         <TouchableOpacity
