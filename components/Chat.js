85c874ffb62ff4515a8429c01f76d268dbfdb97a
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 10:40:33 +0300

    Rokas

---
+++ components/Chat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Chat = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default Chat;
+
+const styles = StyleSheet.create({
+
+});
