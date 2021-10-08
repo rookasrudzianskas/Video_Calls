54eb04625ebc5e209bf7686efef2c52c7cbc8429
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 11:48:21 +0300

    Rokas

--- components/ChatHeader.js
+++ components/ChatHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Pressable} from 'react-native';
 
 const ChatHeader = () => {
     return (
         <View style={styles.container}>
-
+            <Pressable>
+                <Text style={styles.buttonText}>Close</Text>
+            </Pressable>
         </View>
     );
 };
