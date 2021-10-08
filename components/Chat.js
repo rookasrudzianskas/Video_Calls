275b40398e7d62fcafb74aa8622cef6b892ac4d4
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 11:46:09 +0300

    Rokas

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
 import ChatHeader from "./ChatHeader";
 
 const Chat = ({setModalVisible}) => {
     return (
-        <View style={styles.container}>
-            <ChatHeader setModalVisible={setModalVisible} />
-        </View>
+        <SafeAreaView>
+            <View style={styles.container}>
+                <ChatHeader setModalVisible={setModalVisible} />
+            </View>
+        </SafeAreaView>
     );
 };
 
