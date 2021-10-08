6ec08dd128a69a3dc8c8ae78498d7dfc594da7e2
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 10:45:46 +0300

    Rokas

---
+++ components/ChatHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const ChatHeader = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default ChatHeader;
+
+const styles = StyleSheet.create({
+
+});
